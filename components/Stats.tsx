145097aa6865740bf36ec87f97b6e68eac7a6ae1
Miklos Gabor <gmiki91@gmail.com>
2022-03-22 16:34:16 +0100

    profile tabs

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
-import{Snackbar} from 'react-native-paper';
+import { Snackbar } from 'react-native-paper';
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { useAuth } from "../context/AuthContext";
 import { User } from "../models/User";
-import { Form } from './UI/Form';
 import moment from 'moment';
-import {BackButton} from './UI/BackButton';
+import { BackButton } from './UI/BackButton';
 import { ClayTablet } from "./UI/ClayTablet";
+import { Color } from "../Global";
 type ParamList = {
     Params: {
         userId: string
@@ -36,7 +36,7 @@ type PageData = {
 }
 
 const LOCAL_HOST = 'https://8t84fca4l8.execute-api.eu-central-1.amazonaws.com/dev/api';
-const Stats: React.FC<Props> = ({ userProp, children }) => {
+const Stats: React.FC<Props> = ({ userProp }) => {
     const { params } = useRoute<RouteProp<ParamList, 'Params'>>();
     const { token } = useAuth();
     const headers = { Authorization: `Bearer ${token}` };
@@ -46,59 +46,60 @@ const Stats: React.FC<Props> = ({ userProp, children }) => {
     const [storyData, setStoryData] = useState<StoryData>({} as StoryData);
     const [pageData, setPageData] = useState<PageData>({} as PageData);
     useEffect(() => {
-        let mounted=true
+        let mounted = true
         if (params) {
             axios.get(`${LOCAL_HOST}/users/user/${params.userId}`, { headers })
                 .then(result => {
-                    if(mounted)
-                    setUser(result.data.user);
+                    if (mounted)
+                        setUser(result.data.user);
                 })
-                .catch(e=>setError('Error while loading the user data.'));
+                .catch(e => setError('Error while loading the user data.'));
         } else {
-            if(mounted)
-            setUser(userProp)
+            if (mounted)
+                setUser(userProp)
         }
         return () => { mounted = false }
-    }, [params,userProp]);
+    }, [params, userProp]);
 
     useEffect(() => {
         let mounted = true;
         if (user) {
             axios.get(`${LOCAL_HOST}/stories/many/${user._id}`, { headers })
-                .then(result =>{ 
-                   if(mounted) 
-                    setStoryData({
-                    size: result.data.size,
-                    totalVotes: result.data.totalVotes,
-                    upVotes: result.data.upVotes
-                })})
-                .catch(e=>setError('Error while loading the user data.'));
+                .then(result => {
+                    if (mounted)
+                        setStoryData({
+                            size: result.data.size,
+                            totalVotes: result.data.totalVotes,
+                            upVotes: result.data.upVotes
+                        })
+                })
+                .catch(e => setError('Error while loading the user data.'));
             axios.get(`${LOCAL_HOST}/pages/all/${user._id}`, { headers })
                 .then(result => {
-                    if(mounted) 
-                    setPageData({
-                        size: result.data.size,
-                        totalVotes: result.data.totalVotes,
-                        upVotes: result.data.upVotes,
-                        langInfo: result.data.langInfo
-                    })
+                    if (mounted)
+                        setPageData({
+                            size: result.data.size,
+                            totalVotes: result.data.totalVotes,
+                            upVotes: result.data.upVotes,
+                            langInfo: result.data.langInfo
+                        })
                 })
-                .catch(e=>setError('Error while loading the user data.'));
+                .catch(e => setError('Error while loading the user data.'));
         }
         return () => { mounted = false }
     }, [user])
     const storyRating = storyData.totalVotes !== 0 ? `${(storyData.upVotes / storyData.totalVotes * 100).toFixed()}%` : null
     const pageRating = pageData.totalVotes !== 0 ? `${(pageData.upVotes / pageData.totalVotes * 100).toFixed()}%` : null
+    const notProfile = !userProp ? { flex:1 } : null;
     return user ?
-        <View style={styles.container}>
-               
-            <Form>
+        <View style={[styles.container, notProfile]}>
+            <View style={styles.card}>
                 <View style={styles.group}>
                     <Text style={{ fontSize: 36, }}>{user.name}</Text>
                     <Text style={{ fontSize: 12, }}>{user.email}</Text>
                     <Text style={{ fontSize: 12, }}>Last activity: {moment.utc(user.lastActivity).local().startOf('seconds').fromNow()}</Text>
                     {!user.active && <Text style={{ fontSize: 12, }}>Inactive user</Text>}
-                  <Text >{user.numberOfTablets} x <ClayTablet/></Text>
+                    <Text >{user.numberOfTablets} x <ClayTablet /></Text>
                 </View>
                 <View style={styles.group}>
                     <Text >Stories: {storyData.size}</Text>
@@ -113,9 +114,10 @@ const Stats: React.FC<Props> = ({ userProp, children }) => {
                         <Text>Used languages:</Text>
                         {pageData.langInfo?.map(obj => <Text key={obj.language}>{obj.language}: {obj.ratio}% - {obj.level}</Text>)}
                     </View>}
-                {children}
-            </Form>
-            {!userProp &&<BackButton/>}
+        
+            </View>
+            {!userProp && <BackButton />}
+
             <Snackbar onDismiss={() => setError('')} visible={error !== ''} duration={4000}>{error}</Snackbar>
         </View>
         : <Text>Loading</Text>
@@ -123,9 +125,24 @@ const Stats: React.FC<Props> = ({ userProp, children }) => {
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: '15%',
-        flex:1
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+    card: {
+        width: '90%',
+        padding: '5%',
+        backgroundColor: Color.storyCard,
+        borderWidth: 1,
+        borderRadius:10,
+        elevation: 10,
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+    },
+
     group: {
         marginBottom: 15
     }
--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,17 +1,17 @@
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { View, Text, ScrollView} from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Alert } from 'react-native';
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { User } from "../models/User";
 import Stats from "../components/Stats";
 import { CustomInput } from "../components/UI/CustomInput";
-import { Button, Snackbar } from "react-native-paper";
+import { Button, IconButton, Snackbar } from "react-native-paper";
 import { Color } from "../Global";
 import { useIsFocused } from "@react-navigation/native";
-import Logout from "../authentication/Logout";
 
 const LOCAL_HOST = 'https://8t84fca4l8.execute-api.eu-central-1.amazonaws.com/dev/api';
+type Tab = 'Notifications'|'Stats' | 'Settings' | 'Logout';
 const Profile = () => {
     const { token, setToken } = useAuth();
     const headers = { Authorization: `Bearer ${token}` };
@@ -21,14 +21,15 @@ const Profile = () => {
     const [newPassword, setNewPassword] = useState('');
     const [response, setResponse] = useState('');
     const isFocused = useIsFocused();
+    const [tab, setTab] = useState<Tab>('Settings');
     useEffect(() => {
         let mounted = true;
-        if(isFocused){
-        axios.get(`${LOCAL_HOST}/users/`, { headers })
-            .then(result => {
-                if (mounted)
-                    setUser(result.data.user);
-            }).catch(error=> setResponse(error.response.data.message));
+        if (isFocused) {
+            axios.get(`${LOCAL_HOST}/users/`, { headers })
+                .then(result => {
+                    if (mounted)
+                        setUser(result.data.user);
+                }).catch(error => setResponse(error.response.data.message));
         }
         return () => { mounted = false }
     }, [isFocused]);
@@ -43,7 +44,7 @@ const Profile = () => {
     }
 
     const handleDeleteUser = () => {
-       
+
         axios.patch(`${LOCAL_HOST}/users/`, { deletePassword }, { headers })
             .then(() => {
                 axios.delete(`${LOCAL_HOST}/stories/all`, { headers }).catch(error => setResponse(error.response.data.message));
@@ -52,26 +53,87 @@ const Profile = () => {
             })
             .catch(error => setResponse(error.response.data.message));
     }
+    const handleLogout = () => {
+        setTab('Logout');
+        Alert.alert(
+            "Logging out",
+            "",
+            [
+                {text: "Cancel",onPress: () => setTab('Notifications')},
+                { text: "OK", onPress: () => confirmed() }
+            ]
+        );
+    }
 
-    return user?
-        <ScrollView>
-            <Stats userProp={user}>
-                <Logout/>
-                <View style={{ paddingTop: 20, marginTop: 20 }}>
-                    <Text>Update password:</Text>
-                    <CustomInput secureTextEntry value={currentPassword} onChangeText={setCurrentPassword} placeholder='Current password' />
-                    <CustomInput secureTextEntry value={newPassword} onChangeText={setNewPassword} placeholder='New password' />
-                    <Button mode="outlined" color={Color.button} onPress={handlePasswordChange}>Update</Button>
+    const confirmed = async () => {
+        setToken(undefined);
+        await AsyncStorage.removeItem('token');
+    }
+
+    return user ?
+        <View style={{ flex: 1, flexDirection: 'column', marginTop: '15%' }}>
+            <ScrollView >
+                <View style={styles.header}>
+                    <IconButton icon="alert-octagram" onPress={() => setTab('Notifications')} style={{ marginLeft: '2%' }} color={tab === 'Notifications' ? Color.cancelBtn : Color.button} />
+                    <IconButton icon="cogs" onPress={() => setTab('Settings')} style={{ marginLeft: '2%' }} color={tab === 'Settings' ? Color.cancelBtn : Color.button} />
+                    <IconButton icon="chart-bar" onPress={() => setTab('Stats')} style={{ marginLeft: '2%' }} color={tab === 'Stats' ? Color.cancelBtn : Color.button} />
+                    <IconButton icon="exit-to-app" onPress={handleLogout} style={{ marginLeft: '2%' }} color={tab === 'Logout' ? Color.cancelBtn : Color.button} />
                 </View>
-                <View style={{ marginTop: 30 }}>
-                    <Text>Delete account:</Text>
-                    <CustomInput secureTextEntry value={deletePassword} onChangeText={setDeletePassword} placeholder='Current password' />
-                    <Button mode="outlined" color={Color.button} onPress={handleDeleteUser}>Delete</Button>
-                </View>
-                <Snackbar onDismiss={() => setResponse('')} visible={response !== ''} duration={3000}>{response}</Snackbar>
-            </Stats>
-        </ScrollView>
-        :  <Text>Loading</Text>
+                {tab === 'Logout' && <Text style={styles.goodbye}> Good-bye! </Text>}
+                {tab === 'Stats' && <Stats userProp={user}> </Stats>}
+                {tab === 'Settings' && <View style={styles.container}>
+                    <View style={styles.card}>
+                        <Text>Update password:</Text>
+                        <CustomInput secureTextEntry value={currentPassword} onChangeText={setCurrentPassword} placeholder='Current password' />
+                        <CustomInput secureTextEntry value={newPassword} onChangeText={setNewPassword} placeholder='New password' />
+                        <Button color={Color.button} onPress={handlePasswordChange}>Update</Button>
+                        <Text>Delete account:</Text>
+                        <CustomInput secureTextEntry value={deletePassword} onChangeText={setDeletePassword} placeholder='Current password' />
+                        <Button color={Color.button} onPress={handleDeleteUser}>Delete</Button>
+                    </View>
+                    <Snackbar onDismiss={() => setResponse('')} visible={response !== ''} duration={3000}>{response}</Snackbar>
+                </View>}
+            </ScrollView>
+        </View>
+        : <Text>Loading</Text>
 }
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: '5%',
+        marginRight: '5%',
+        borderWidth: 1,
+        backgroundColor: Color.storyCard
+    },
+
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    card: {
+        width: '90%',
+        padding: '5%',
+        backgroundColor: Color.storyCard,
+        borderWidth: 1,
+        borderRadius:10,
+        elevation: 10,
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+    },
+    goodbye:{ 
+        fontSize:64,
+        color:Color.secondary,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+    }
+})
 
 export default Profile;
