dafc438c797f3e8ebdab1d9b1a69680204d77160
Miklos Gabor <gmiki91@gmail.com>
2022-04-11 16:17:10 +0200

    less languages

--- components/About.tsx
+++ components/About.tsx
@@ -8,6 +8,6 @@ const About = () => (
         <Text style={{textAlign:'left'}}>3. See who contributed to your story and add the pages you like the most</Text>
         <Text style={{marginTop:'5%', marginBottom:'5%'}}>⭐Complete your daily contribution 3 times to get an extra page</Text>
         <Text>⭐Have 3 page accepted to get a new story</Text>
-        <Text style={{marginTop:'10%', textAlign:'center'}}>Got any questions or feedback? Send me an email: admin@glyphses.com</Text>
+        <Text style={{marginTop:'10%', textAlign:'center'}}>Got any questions or feedback? Or want a language added? Send an email: admin@glyphses.com</Text>
     </View>)
 export default About;
